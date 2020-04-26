c23a02c8e4236bc93725e9d5005a1e5ef33b8883
Chue Yang <chueyangib@gmail.com>
2020-04-25 20:14:38 -0500

    All funcitonality except timer.

--- asset/script.js
+++ asset/script.js
@@ -1,8 +1,10 @@
 var startButton = document.querySelector("#start");
 var countEl = document.getElementById("count");
 var quizContainer = document.getElementById("quiz");
+var content = document.getElementById("content");
 var resultsContainer = document.getElementById('results');
 var submitButton = document.getElementById('submit');
+var initialSubmit;
 
 var numCorrect  = 0;
 
@@ -14,9 +16,7 @@ function timerStart(){
     var timeInterval = setInterval (function(){
         countEl.textContent = timeLeft;
         timeLeft--;
-        //generateQuiz(Questions, quizContainer, answers, submitButton);
-        //we may need to also and an if answer is wrong 
-        //where is the best place to put wrong answers
+       
         if (timeLeft <= 0){
             timeLeft.textContent = " ";
             alert ("You're done with the quiz!");
@@ -73,8 +73,8 @@ var currentQuestion = 0;
 function showOne(questionsList, questionNum, quizContainer){
     var output =[];
     var answers =[];
-    
-    //Display radio buttons
+
+      
     for (letter in questionsList[questionNum].answers){
         answers.push(            
             '<label>'
@@ -94,7 +94,7 @@ function showOne(questionsList, questionNum, quizContainer){
    output.push(
        ' <button id ="submit" onclick = "checkAnswer(' + questionNum + ')"> Submit answer</button>  '
     );
-
+   
     quizContainer.innerHTML = output.join('');
 }
 
@@ -103,18 +103,6 @@ function showNextQuestion(questionNum){
         showOne(myQuestions, questionNum+1, quizContainer);
     }
 
-}
-function showResults(){
-    var output=[];
-
-    output.push(
-        '<div class="Finish">All done!</div>'
-        + '<p>Your score is'+numCorrect+'</p>'
-        +'<label for="initials">Enter initials:</label>'
-        + '<input type="initials" placeholder="supersecure123"/>' 
-        +'<button id="initialSubmit">Submit</button>'
-    );
-    quizContainer.innerHTML = output.join('');
 }
 
 function checkAnswer(questionNum) {
@@ -132,12 +120,111 @@ function checkAnswer(questionNum) {
     }
 
     showNextQuestion(questionNum);
-    
+
     if(questionNum == myQuestions.length -1){
         showResults();
     }
 }
 
 startButton.addEventListener("click", function(){
+//Hide Div "Coding throiugh start quiz"
+    // var scores=[];
+    // scores.push('');
+    // content.innerHTML = scores.join('');
+    toggleContentDiv();
     showOne(myQuestions, 0, quizContainer);
+    
 });
+
+function showResults(){
+    var output=[];
+
+    output.push(
+        '<h1> All done!</h1>'
+        +'<p>Your score is '+numCorrect+'</p>'
+        + '<form id="frm1">'
+        +  'Enter your name: <input type="text" id="name"><br>'
+        + '<input type="button" value="Submit your score!" id = "submitResults" > '
+        +'</form>'
+
+    );
+    quizContainer.innerHTML = output.join('');
+    initialSubmit = document.getElementById('submitResults');
+
+    initialSubmit.addEventListener("click", function(event){
+        event.preventDefault();
+        var initials =document.getElementById("name").value;
+        console.log("initials from input: " +initials);
+    
+        if (initials ===""){
+            displayMessage("Initials cannot be blank");
+        } else {
+            localStorage.setItem("name", initials);
+            localStorage.setItem("score", numCorrect);
+    
+            console.log('name from local storage in showResults: ' + localStorage.getItem("name"));
+            console.log('score from local storage in showResults: '+ localStorage.getItem("score"));
+
+            highScoreRegistered();
+        }
+    
+       
+    
+    });
+}
+
+function displayMessage(message){
+    content.textContent=message;
+//    startButtonDiv.setAttribute("", );
+}
+
+function highScoreRegistered(){
+
+    var scores=[];
+    var name = localStorage.getItem("name");
+    var highScore = localStorage.getItem("score");
+    
+    console.log('Name from local storage in highscores: ' + name);
+    scores.push(
+        '<h1> Highscores</h1>'
+        +'<p>' + highScore + '-' + name + '</p>'
+        +'<button id= "goBack" onclick = "goBack()">  Go Back </button> '
+        +'<button id="clearScores" onclick="clearScores()"> Clear Highscores </button> '
+    );
+  quizContainer.innerHTML = scores.join('');
+}
+
+function goBack(){
+    
+    //Hides high score info.
+    var scores=[];
+    quizContainer.innerHTML = scores.join('');
+
+    //Puts the quiz directions and start button back.
+    toggleContentDiv();
+  
+}
+
+function toggleContentDiv(){
+    var hide = document.getElementById("content");
+    console.log("toggleDiv: " + hide);
+
+    if (hide.style.display ==="none"){
+        hide.style.display ="block";
+    }else{
+        hide.style.display="none";
+    }
+}
+
+function clearScores(){
+    var scores=[];
+    
+    scores.push(
+        '<h1> Highscores</h1>'
+        +'<p> All scores cleared</p>'
+        +'<button id= "goBack" onclick = "goBack()">  Go Back </button> '
+        +'<button id="clearScores" > Clear Highscores </button> '
+    );
+    quizContainer.innerHTML = scores.join('');
+
+}
